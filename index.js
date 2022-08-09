d1c87939dc2af18296dbdea8b70404923ee96642
Andrey <47743995+andreiesin96159@users.noreply.github.com>
2022-08-09 17:15:11 +0300

    12345

--- index.js
+++ index.js
@@ -4,17 +4,19 @@ var navMain = document.querySelector('.navigation-list');
     var navToggle = document.querySelector('.navigation__toggle');
     var navHeader = document.querySelector('.header');
     var navToggleClose = document.querySelector('.navigation-button--mobile');
-
-
+     var description = document.querySelector('.description');
+ 
  
     navToggle.addEventListener('click', function () {
         if (navMain.classList.contains('navigation-list--close')) {
             navMain.classList.remove('navigation-list--close');
+            description.classList.add('description--none');
             navMain.classList.add('navigation-list--active');
            
         } else {
             navMain.classList.add('navigation-list--close');
             navMain.classList.remove('navigation-list--active');
+          
         }
       });
   
@@ -22,5 +24,6 @@ var navMain = document.querySelector('.navigation-list');
         if (navMain.classList.contains('navigation-list--active')) {   
             navMain.classList.add('navigation-list--close');
             navMain.classList.remove('navigation-list--active');
+            description.classList.remove('description--none');
       }
     })
